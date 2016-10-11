5aee0fcf9100ba626efaa1bc047fe3f7fd127157
Marek Counts <countsmarek@gmail.com>
2016-10-10 21:34:07 -0400

    little updates

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ var app = express();
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 app.use(require('less-middleware')(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '')));
 
 app.use('/', routes);
 
